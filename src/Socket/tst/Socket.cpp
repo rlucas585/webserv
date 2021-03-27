9003e7a4930f6d243bb226f3c9bb0efc501ae1a4
Ryan Lucas <rlucas@student.codam.nl>
2021-03-27 17:11:41 +0100

    Utils::pair added. SocketAddr & Socket edited
    * Utils::pair is a simple recreation of the std::pair class from the
    standard library, capable of holding any two types.
    * Testing for Utils::pair is minimal, but it's identical to the one that
    was submitted for my ft_containers project, and I saw no issues there
    while using it with a custom-created ft::map class.
    * SocketAddrV4::into_inner() now returns a pair, similar to SocketAddr
    from Rust std library.
    * Socket read, recv_with_flags, and peek functions added. Not tested
    sufficiently yet.
    * A test using the Socket class, binding and listening, was successful
    using netcat. I'm going to have to work on a more extensive solution,
    probably using threads/forks, to automate this testing.

--- src/Socket/tst/Socket.cpp
+++ src/Socket/tst/Socket.cpp
@@ -6,7 +6,7 @@
 /*   By: rlucas <marvin@codam.nl>                     +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/27 13:18:54 by rlucas        #+#    #+#                 */
-/*   Updated: 2021/03/27 17:06:25 by rlucas        ########   odam.nl         */
+/*   Updated: 2021/03/27 17:10:45 by rlucas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,23 +55,23 @@ TEST(Socket_tests, move_semantics_test) {
 
 // This test requires a manual 'nc' command currently.
 // TODO: Automate this test
-TEST(Socket_tests, bind_test) {
-    SocketAddrV4 addr = SocketAddrV4::init("localhost:4242");
-    Socket socket = Socket::init(addr, SOCK_STREAM);
-    int ret;
-
-    Utils::pair<const sockaddr*, socklen_t> addr_info = addr.into_inner();
-    const sockaddr* addrp = addr_info.first;
-    socklen_t len = addr_info.second;
-    ret = bind(socket.into_inner(), addrp, len);
-    if (ret == -1) {
-        throw Utils::runtime_error("Error binding socket");
-    }
-    ret = listen(socket.into_inner(), 128); // 128 is the backlog
-    if (ret == -1) {
-        throw Utils::runtime_error("Error listening from socket");
-    }
-    sockaddr_storage storage;
-    Utils::memset(&storage, 0, sizeof(storage));
-    Socket client = socket.accept(reinterpret_cast<sockaddr*>(&storage), &len);
-}
+// TEST(Socket_tests, bind_test) {
+//     SocketAddrV4 addr = SocketAddrV4::init("localhost:4242");
+//     Socket socket = Socket::init(addr, SOCK_STREAM);
+//     int ret;
+//
+//     Utils::pair<const sockaddr*, socklen_t> addr_info = addr.into_inner();
+//     const sockaddr* addrp = addr_info.first;
+//     socklen_t len = addr_info.second;
+//     ret = bind(socket.into_inner(), addrp, len);
+//     if (ret == -1) {
+//         throw Utils::runtime_error("Error binding socket");
+//     }
+//     ret = listen(socket.into_inner(), 128); // 128 is the backlog
+//     if (ret == -1) {
+//         throw Utils::runtime_error("Error listening from socket");
+//     }
+//     sockaddr_storage storage;
+//     Utils::memset(&storage, 0, sizeof(storage));
+//     Socket client = socket.accept(reinterpret_cast<sockaddr*>(&storage), &len);
+// }
